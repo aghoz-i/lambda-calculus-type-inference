3e845428642cc493800059e9a997d53529a3d789
Taufik Algi F <taufik.algi99@gmail.com>
2023-03-27 23:23:46 +0700

    [WIP] parser

--- parse.pl
+++ parse.pl
@@ -9,3 +9,12 @@
 % <expr> ::= <var> | <lambda> <var> '.' <expr> | '(' <expr> ')' | <expr> <expr> ; currently left recursive need to be adjusted to be acceptable in DCG
 % <var> ::= [a-z][0-9]*
 % <lambda> ::= '\' | ''λ'
+
+% \s z -> (s z) === (a -> a) -> a -> a
+
+'C'('λ', [X|L], '.', L).
+
+parse(X, Remain) :- var(X, Remain).
+parse(X, Remain) :- var(X, X1), 'C'(λ, X1, ., X2), parse(X2, Remain).
+parse(X, Remain) :- var(X, X1), 'C'(\, X1, ., X2), parse(X2, Remain).
+var([X|Remain], Remain) :- var(X).
